82e9b84c800cce6b3d1bf1e0aebd43a3a26db911
Nextus Marketing <marketingteamnextus@gmail.com>
2025-11-06 00:40:55 +0530

    changes done

--- resources/views/Admin/Enquiries/show.blade.php
+++ resources/views/Admin/Enquiries/show.blade.php
@@ -1,49 +1,42 @@
 <div class="row align-items-center">
     <div class="col-lg-8 order-last">
         <ul class="list-unstyled mb-4">
-
             <li class="d-flex align-items-center gap-3 mb-4">
                 <i class="ti ti-user text-dark fs-6"></i>
                 <h6 class="fs-5 mb-0">
                     Full Name : <span class="fw-normal">{{ $enquiry->full_name ?? 'N/A' }}</span>
                 </h6>
             </li>
-
             <li class="d-flex align-items-center gap-3 mb-4">
                 <i class="ti ti-mail text-dark fs-6"></i>
                 <h6 class="fs-5 mb-0">
                     Email : <span class="fw-normal">{{ $enquiry->email ?? 'N/A' }}</span>
                 </h6>
             </li>
-
             <li class="d-flex align-items-center gap-3 mb-4">
                 <i class="ti ti-phone text-dark fs-6"></i>
                 <h6 class="fs-5 mb-0">
                     Mobile : <span class="fw-normal">{{ $enquiry->mobile ?? 'N/A' }}</span>
                 </h6>
             </li>
-
             <li class="d-flex align-items-center gap-3 mb-4">
                 <i class="ti ti-book text-dark fs-6"></i>
                 <h6 class="fs-5 mb-0">
                     Subject : <span class="fw-normal">{{ $enquiry->subject ?? 'N/A' }}</span>
                 </h6>
             </li>
-
             <li class="d-flex align-items-center gap-3 mb-4">
                 <i class="ti ti-briefcase text-dark fs-6"></i>
                 <h6 class="fs-5 mb-0">
                     Profession : <span class="fw-normal">{{ $enquiry->profession ?? 'N/A' }}</span>
                 </h6>
             </li>
-
             <li class="d-flex align-items-center gap-3 mb-4">
                 <i class="ti ti-info-circle text-dark fs-6"></i>
                 <h6 class="fs-5 mb-0">
                     Enquiry Type : <span class="fw-normal">{{ $enquiry->enquiry_type ?? 'N/A' }}</span>
                 </h6>
             </li>
-
             <li class="d-flex align-items-center gap-3 mb-4">
                 <i class="ti ti-clock text-dark fs-6"></i>
                 <h6 class="fs-5 mb-0">
