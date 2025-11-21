a2ab58751e7889c16b605ef0b990cde50759367e
Nextus Marketing <marketingteamnextus@gmail.com>
2025-11-21 21:31:17 +0530

    done

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -97,7 +97,7 @@
             </div>
             <div class="header-btn d-inline-flex">
               <a href="tel:+551-348-1040" class="btn-default btn-highlighted">
-                Call Us Now
+                Call Us Nows
               </a>
             </div>
 					</div>
